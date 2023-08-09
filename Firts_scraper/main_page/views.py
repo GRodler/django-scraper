039b6125f219555f84b8701d16294085db479248
guido <guido.rodler@buonarroti.tn.it>
2023-08-09 23:17:49 +0200

    fix:now the message for resume is given only when a new insertion is made

--- Firts_scraper/main_page/views.py
+++ Firts_scraper/main_page/views.py
@@ -5,6 +5,8 @@ from django.http.response import HttpResponse,HttpResponseRedirect
 from .forms import AgeForm
 from .models import User_info
 # Create your views here.
+
+new_insertion = False #used for undertanding if the resume page is requested from the home or after insering a new value in the db
 def home(request):
     return render(request,'index.html')
 
@@ -12,6 +14,7 @@ def saveData(form):
     form = form.cleaned_data  # return a dictionary with the data in normal python types
     d = User_info(name=form['name'],age=form['age'],extra=form['info'])
     d.save()#django does not touch the database until you call the save method
+    globals()['new_insertion'] = True #globals gives a dictionary of all the variables in the global of the python program
 
 def check_if_exist(form):#checks if there alredy is someone with the same name
     found = False
@@ -40,4 +43,8 @@ def take_info(request):#while im in this page this view generate an empty form t
     return render(request, 'information/little_form.html', {'form':form})
 def give_info(request):
     query = User_info.objects.all()#values takes the value and convert into a dict
+    if new_insertion: #when is searching a variable python first chech the inner fuction after that an external function (if exist ) and after that outide of the function
+        messages.success(request, 'Congratulation new info have been stored')
+        globals()['new_insertion'] = False
+
     return render(request,'information/info.html', {'query': query},)
